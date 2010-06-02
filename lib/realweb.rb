58a23876a3e5a2722b46b871ad7e87e0d2f53112
Martin Emde <martin.emde@gmail.com>
2010-06-02 16:07:50 -0700

    Alias the method after it's defined (oops)

--- lib/realweb.rb
+++ lib/realweb.rb
@@ -6,19 +6,19 @@ module RealWeb
     def start_server_in_thread(*args)
       InThreadServer.new(*args) { |server| server.start }
     end
-    alias start_server start_server_in_fork
 
     def start_server_in_fork(*args)
       ForkingServer.new(*args) { |server| server.start }
     end
+    alias start_server start_server_in_fork
 
     def with_server_in_thread(*args, &block)
       InThreadServer.with_rackup(*args, &block)
     end
-    alias with_server with_server_in_fork
 
     def with_server_in_fork(*args, &block)
       ForkingServer.with_rackup(*args, &block)
     end
+    alias with_server with_server_in_fork
   end
 end
