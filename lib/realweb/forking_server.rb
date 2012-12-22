46a8fa0142c50acdcab02e43fef83e8bd9685abc
Martin Emde <martin.emde@gmail.com>
2012-12-21 18:28:12 -0800

    Make pid killing a bit more graceful
    We used to just flood a bunch of signals at the server,
    now we allow it to quit on its own and only escalate after
    a small timeout.

--- lib/realweb/forking_server.rb
+++ lib/realweb/forking_server.rb
@@ -3,6 +3,8 @@ require 'realweb/server'
 module RealWeb
   class ForkingServer < Server
 
+    KILL_TIMEOUT = 2.0 # second
+
     def stop
       kill_pid
       super
@@ -10,14 +12,22 @@ module RealWeb
 
     protected
 
-    def kill_pid
+    # Kill, with increasing severity within KILL_TIMEOUT
+    # Tries each signal, giving equal time for each signal to work.
+    def kill_pid(signals = [:INT, :TERM, :KILL], timeout = KILL_TIMEOUT / signals.size.to_f)
       return unless @pid
-      Process.kill 'INT', @pid
-      Process.kill 'TERM', @pid
-      Process.wait @pid
-    rescue
-      # noop
-    ensure
+      signal, *next_signals = *signals
+      Process.kill signal, @pid
+      Timeout.timeout(timeout) { Process.wait @pid }
+      @pid = nil
+    rescue Timeout::Error
+      if next_signals.empty?
+        raise Timeout::Error, "Unable to kill server process within #{KILL_TIMEOUT} seconds (amazingly)"
+      end
+
+      kill_pid(next_signals, timeout)
+    rescue Errno::ECHILD, Errno::ESRCH
+      # process doesn't exist
       @pid = nil
     end
 
